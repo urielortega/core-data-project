13b22cb8470c90a8f5d3314ab9f5c12c64e63bd0
Uriel Ortega <urielortega2509@gmail.com>
2023-06-05 10:05:47 -0600

    Initial Commit

---
+++ CoreDataProject/CoreDataProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreDataProjectApp.swift
+//  CoreDataProject
+//
+//  Created by Uriel Ortega on 05/06/23.
+//
+
+import SwiftUI
+
+@main
+struct CoreDataProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
